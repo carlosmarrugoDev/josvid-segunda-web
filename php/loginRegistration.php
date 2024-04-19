f5c58e9cdc9b8a3ff8d747c161cf764ceef9500c
Carlos <marrugovargascarlos@gmail.com>
2024-04-19 10:00:02 -0500

    Password validation

--- php/loginRegistration.php
+++ php/loginRegistration.php
@@ -5,12 +5,14 @@ include 'database.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$password_incraptado = hash('123',$password);
+$password = hash('sha512',$password);
 
 $query = "INSERT INTO useraccount(name, email, password)
               VALUES('$name', '$email', '$password')";
 
 $ejecutar = mysqli_query($conexion, $query);
+
+$verificar_correo = mysqli_query($conexion, "SELECT * FROM useraccount WHERE email =  '$email'");
 
 if ($ejecutar) {
     echo '
