472d25463be20ae0f87eeb6f5480883ec5da9ca2
Carlos <marrugovargascarlos@gmail.com>
2024-04-21 10:47:09 -0500

    cambie pagos.html a pagos.php para validar

--- php/loginValidation.php
+++ php/loginValidation.php
@@ -1,30 +1,30 @@
 <?php
-    session_start();
-    include 'database.php';
+session_start();
+include 'database.php';
 
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-    $validar_login = mysqli_query($conexion, "SELECT * FROM useraccount 
+$validar_login = mysqli_query($conexion, "SELECT * FROM useraccount 
     WHERE email = '$email' and password = '$password'");
 
-    if(mysqli_num_rows($validar_login) > 0){
-        $query = "CREATE VIEW USERTEM as SELECT * FROM usercourses WHERE Id_user = (SELECT id FROM useraccount 
+if (mysqli_num_rows($validar_login) > 0) {
+    $query = "CREATE VIEW USERTEM as SELECT * FROM usercourses WHERE Id_user = (SELECT id FROM useraccount 
         WHERE email = '$email' and password = '$password')";
-        mysqli_query($conexion, $query);
-            $_SESSION['user'] = $email;
-            echo '
+    mysqli_query($conexion, $query);
+    $_SESSION['user'] = $email;
+    echo '
                 <script>
                     window.location = "../courses.html";
                 </script>
                 exit;
             ';
-    } else {
-        echo '
+} else {
+    echo '
             <script>
                 alert("No eres tu, Somos Nosotros!");
                 windows.location = "../login.html";
             </script>
             exit;
         ';
-    }
+}
